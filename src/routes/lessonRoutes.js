bb030f6c2a4b75eb2342682a1931d16a05d2f05f
MohdAsif2 <moh12dasif@gmail.com>
2026-09-16 18:58:10 +0530

    Delete src directory

--- src/routes/lessonRoutes.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const lessonController = require('../controllers/lessonController');
-
-router.get('/current', lessonController.getCurrentLesson); // NOT '/api/lessons/current'
-
-module.exports = router;
